c9946e17abda27df014ffea7bf6b005d2b5bc0ad
Berkovich Pavel <biarkovich.pavel@gmail.com>
2020-12-12 17:47:07 +0300

    add some fixes to previous commit

--- app/jobs/find_items_for_users_job.rb
+++ app/jobs/find_items_for_users_job.rb
@@ -5,10 +5,10 @@ class FindItemsForUsersJob < ApplicationJob
   def perform(user_id)
     subscription = Subscription.where(user_id: user_id).first
     last_sent = subscription.last_sent
-    subscription.last_sent = Time.current
     items = Item.where(status: 'active', updated_at: last_sent..Time.current)
     items_id = items.map(&:id)
     subscription.last_sent = Time.current
+    subscription.save
     SendItemsToUserJob.perform_now(user_id, items_id)
   end
 end
