d258804e7fea198fe335c8137ea273e885b8738b
Berkovich Pavel <biarkovich.pavel@gmail.com>
2021-01-08 21:39:58 +0300

    refactor parser

--- lib/rss_parser.rb
+++ lib/rss_parser.rb
@@ -14,12 +14,14 @@ class RssParser
     rss_results = []
     rss = RSS::Parser.parse(open("#{@url}").read, false)
     rss.items.each do |result|
+      href = result.enclosure.url
+      href[-3..-1] = 'jpg' if result.enclosure.url[-3..-1] == 'mp4'
       result = {
         title: result.title,
         short_description: "Details on the link: #{link_to result.link, result.link}",
         full_text: result.description,
         reference_link: result.link,
-        main_img: result.enclosure.url
+        main_img: href
       }
       rss_results.push(result)
     end
