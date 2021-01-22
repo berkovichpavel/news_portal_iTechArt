070ed54f686478673e979c480111e35c11050888
Berkovich Pavel <biarkovich.pavel@gmail.com>
2021-01-22 23:59:43 +0300

    Add user auth with email confirmation

--- app/models/user.rb
+++ app/models/user.rb
@@ -17,6 +17,7 @@ class User < ApplicationRecord
          :recoverable,
          :rememberable,
          :validatable,
-         :trackable # keeps statistics of the number of entries, takes into account time and IT addresses.
+         :trackable,
+         :confirmable # keeps statistics of the number of entries, takes into account time and IT addresses.
   # :omniauthable # authentication using social networks
 end
