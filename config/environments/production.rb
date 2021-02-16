39efa73ef0152928bd0cf6b3874c12536803c2a7
Berkovich Pavel <biarkovich.pavel@gmail.com>
2021-02-16 22:04:30 +0300

    update prod.rb for heroku

--- config/environments/production.rb
+++ config/environments/production.rb
@@ -80,6 +80,11 @@ Rails.application.configure do
   # require 'syslog/logger'
   # config.logger = ActiveSupport::TaggedLogging.new(Syslog::Logger.new 'app-name')
 
+  config.cache_classes = true
+  config.serve_static_assets = true
+  config.assets.compile = true
+  config.assets.digest = true
+
   if ENV['RAILS_LOG_TO_STDOUT'].present?
     logger           = ActiveSupport::Logger.new(STDOUT)
     logger.formatter = config.log_formatter
